a31d724e1d19c700a5d8ecef34c2a5435836c3c3
Yaut <yautrose@gmail.com>
2022-09-06 12:48:59 +0300

    Add new functionality

---
+++ Flash Chat iOS13/Controllers/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Flash Chat iOS13
+//
+//  Created by Alexey Artyushenko on 06.09.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -16,6 +16,7 @@ class WelcomeViewController: UIViewController {
             }
             charIndex += 1
         }
+
     }
     
 }
